fca8e6f39c7a1169bc7709058adf7af36b36cd26
ehek01 <whdgus0515@gmail.com>
2022-08-08 11:06:46 +0900

    Repository 환경 세팅 완료!!

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # JUnit5
+테스트 진행순서
+
+Repository => Service => Web
+
+---
+Controller : 클라이언트와 테스트(data 요청등)
+
+Service : 기능들이 트랜잭션을 잘 타는지 확인
+
+Repository : DB쪽 관련 테스트
+
+단위 테스트 : 필요한 class 들만 메모리에 올려서 테스트
+
+통합 테스트 : SpringBoot 와 동일한 환경에서 테스트(전부 메모리에 올림)
+
+---
+
+TestCode 작성시엔 @AutoWired로 DI를 시켜주세요.
+-
+
+@DataJpaTest : DB와 관련된 컴포넌트만 메모리에 로딩
+
--- src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
@@ -1,7 +1,27 @@
 package site.metacoding.junitproject.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class BookRepositoryTest {
 
+    @Autowired
+    private BookRepository bookRepository;
+
+    // 1. 책 등록
+    @Test
+    public void book_save_test() {
+        System.out.println("책등록 실행");
+    }
+    // 2. 책 목록보기
+
+    // 3. 책 한건보기
+
+    // 4. 책 수정
+
+    // 5. 책 삭제
 }
