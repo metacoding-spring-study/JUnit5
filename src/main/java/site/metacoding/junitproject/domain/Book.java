60636b9d7fc251b05b73269e65b1ecb34942d274
ehek01 <whdgus0515@gmail.com>
2022-08-08 10:51:06 +0900

    Book Entity 생성완료

--- src/main/java/site/metacoding/junitproject/domain/Book.java
+++ src/main/java/site/metacoding/junitproject/domain/Book.java
@@ -1,4 +1,30 @@
 package site.metacoding.junitproject.domain;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Column(length = 20, nullable = false)
+    private String author;
+
+    @Builder
+    public Book(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
 }
--- src/test/java/site/metacoding/junitproject/web/BookApiControllerTest.java
+++ src/test/java/site/metacoding/junitproject/web/BookApiControllerTest.java
@@ -3,5 +3,7 @@ package site.metacoding.junitproject.web;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookApiControllerTest {
+    public void test() {
 
+    }
 }
